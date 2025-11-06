f27e0e538d330e1fb1e7c9351a75b2a869e565c1
marley <116856059+Marleyedrg@users.noreply.github.com>
2025-11-06 16:10:18 -0300

    fixe num array

--- src/FixedNumberArray.ts
+++ src/FixedNumberArray.ts
@@ -1,16 +1,19 @@
 export default function FixedNumberArray(size: number, values: number[]) {
 
-  const byterByItem = Int32Array.BYTES_PER_ELEMENT;
+  const BytPerInt = Int32Array.BYTES_PER_ELEMENT;
 
   //In this case, on a 32-bit system, we are defining that we want to use 4 bytes to represent each int.
 
   const sizeOfArray: number = size;
 
-  const buffer: ArrayBuffer = new ArrayBuffer(sizeOfArray * byterByItem);
-  //take continous free space that we want
-  //hexadecimal value by index
+  const buffer: ArrayBuffer = new ArrayBuffer(sizeOfArray * BytPerInt);
+  //take continous free space in bytes that we want
+  //hexadecimal value by index ex: 0 is 00
 
   const arr: Int32Array = new Int32Array(buffer);
+
+  buffer.slice(0, 2);
+  console.log(new ArrayBuffer())
 
   for (let i = 0; i < sizeOfArray; i++) {
     arr[i] = values[i];
